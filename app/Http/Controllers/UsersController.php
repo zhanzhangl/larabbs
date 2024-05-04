ae0dd5a0a4f4bccc56cd3e6ad66574037c4f2d81
zhanzhangl <zhanzhangl2023@163.com>
2024-05-04 12:59:00 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,11 @@ use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -17,6 +22,7 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
@@ -26,6 +32,7 @@ class UsersController extends Controller
 
         // $user->update($request->all());
         // return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
