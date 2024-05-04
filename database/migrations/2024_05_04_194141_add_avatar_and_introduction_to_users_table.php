39ad46dc769a0a34e9c837294e2d6291bcda376d
zhanzhangl <zhanzhangl2023@163.com>
2024-05-04 11:54:04 +0000

    编辑个人资料

---
+++ database/migrations/2024_05_04_194141_add_avatar_and_introduction_to_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Schema::table('users', function (Blueprint $table) {
+        //     //
+        // });
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable();
+            $table->string('introduction')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    // public function down()
+    // {
+    //     Schema::table('users', function (Blueprint $table) {
+    //         //
+    //     });
+    // }
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+            $table->dropColumn('introduction');
+        });
+    }
+};
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -34,7 +34,7 @@
                 class="img-responsive img-circle" width="30px" height="30px">
               {{ Auth::user()->name }}
             </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            {{-- <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="">个人中心</a>
               <a class="dropdown-item" href="">编辑资料</a>
               <div class="dropdown-divider"></div>
@@ -44,7 +44,20 @@
                   <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
                 </form>
               </a>
+            </div> --}}
+
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
+              <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" id="logout" href="#">
+                <form action="{{ route('logout') }}" method="POST">
+                  {{ csrf_field() }}
+                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                </form>
+              </a>
             </div>
+
           </li>
         @endguest
       </ul>
