5a4c7a9c8693154f07e466682d7767b9dd914bc6
zhanzhangl <zhanzhangl2023@163.com>
2024-05-05 22:48:13 +0800

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -7,21 +7,31 @@ use App\Models\Topic;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
+// class TopicObserver
+// {
+//     public function creating(Topic $topic)
+//     {
+//         //
+//     }
+
+//     public function updating(Topic $topic)
+//     {
+//         //
+//     }
+
+//     public function saving(Topic $topic)
+//     {
+//         $topic->body = clean($topic->body, 'user_topic_body');
+//         $topic->excerpt = make_excerpt($topic->body);
+//     }
+
+// }
 class TopicObserver
 {
-    public function creating(Topic $topic)
-    {
-        //
-    }
-
-    public function updating(Topic $topic)
-    {
-        //
-    }
-
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');
+
         $topic->excerpt = make_excerpt($topic->body);
     }
-
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 	{
+        // \Carbon\Carbon::setLocale("zh");
+        // \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         \Illuminate\Pagination\Paginator::useBootstrap();
-
-        //
     }
 }
