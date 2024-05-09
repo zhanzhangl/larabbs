4219e98386a287c2c786d674287608d0ab127ed9
zhanzhangl <zhanzhangl2023@163.com>
2024-05-09 16:08:43 +0000

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -83,4 +83,10 @@ class User extends Authenticatable implements MustVerifyEmail
     {
         return $this->id == $model->user_id;
     }
+    public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 }
